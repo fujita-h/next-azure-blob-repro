b0506292e4338de0e984aabd92698ffb36c74742
Jonathan Rupp <jrupp@rightpoint.com>
2023-09-20 13:41:31 -0500

    Add @azure/storage-blob and code to call it

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,29 @@
+import {
+  BlobServiceClient,
+} from '@azure/storage-blob';
+
+// need to set these in your environment to any valid Azure Storage account
+const account = process.env.STORAGE_ACCOUNT_NAME!;
+const accountKey = process.env.STORAGE_ACCOUNT_KEY!;
+const connectionString = `DefaultEndpointsProtocol=https;AccountName=${account};AccountKey=${accountKey};EndpointSuffix=core.windows.net`;
+
+const getBlobServiceClient = () =>
+  BlobServiceClient.fromConnectionString(connectionString);
+
+export const getContainerCount = async () => {
+  console.log('calling azure storage');
+  const blobServiceClient = getBlobServiceClient();
+  const containers = blobServiceClient.listContainers();
+  let count = 0;
+  for await (const _ of containers) {
+      count++;
+  }
+  return count;
+}
+
+export const dynamic = 'force-dynamic'; // don't do SSG
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default async function Home() {
+  return await getContainerCount();
 }
